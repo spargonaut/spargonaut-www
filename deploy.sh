de8b8d9c1f7a96cc19c26c6d75968cb9120a08d5
spargonaut <1243607+spargonaut@users.noreply.github.com>
2015-11-22 20:19:03 -0600

    JS - fix the style compilation destination directory in the deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "compiling less files"
-lessc ./src/less/styles.less ./prod/styles.css
+lessc ./src/less/styles.less ./prod/styles/styles.css
 
 echo "copying html files to prod"
 cp ./src/*.html ./prod/
