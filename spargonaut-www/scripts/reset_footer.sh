3f3c65a93254e796cbe04508223e7856ad901534
spargonaut <1243607+spargonaut@users.noreply.github.com>
2024-08-04 23:01:26 -0700

    create makefile and scripts for generating .wtf public files

---
+++ spargonaut-www/scripts/reset_footer.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+FILES_TO_RESET=$(git diff --name-only)
+
+for FILE in $FILES_TO_RESET; do
+  sed -i 's/<p>to be incredibly lazy<\/p>/<p>working really hard<\/p>/' ${FILE/spargonaut-www\//}
+done
+
+# stage files with more than just whitespace-only changes
+if [[ $(git diff -w --no-color | wc -l) -ne 0 ]]; then
+  git diff -w --no-color | git apply --cached --ignore-whitespace
+fi
+
+# the only files not staged at this point should whitespace only changes
+git checkout -- .
