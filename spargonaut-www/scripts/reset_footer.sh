10657e11e53e7090a08f751b075c9cf25402ff49
spargonaut <1243607+spargonaut@users.noreply.github.com>
2024-08-14 22:44:44 -0700

    reset the footer tag, but leave the root index page alone.

--- spargonaut-www/scripts/reset_footer.sh
+++ spargonaut-www/scripts/reset_footer.sh
@@ -5,7 +5,11 @@ set -euo pipefail
 FILES_TO_RESET=$(git diff --name-only)
 
 for FILE in $FILES_TO_RESET; do
-  sed -i 's/<p>to be incredibly lazy<\/p>/<p>working really hard<\/p>/' ${FILE/spargonaut-www\//}
+  if [ "spargonaut-www/public/index.html" == "${FILE}" ]; then
+    sed -i 's/<p>working really hard<\/p>/<p>to be incredibly lazy<\/p>/' ${FILE/spargonaut-www\//}
+  else
+    sed -i 's/<p>to be incredibly lazy<\/p>/<p>working really hard<\/p>/' ${FILE/spargonaut-www\//}
+  fi
 done
 
 # stage files with more than just whitespace-only changes
